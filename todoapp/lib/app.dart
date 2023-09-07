43e14b9bee0ea728377a4ab96bad89316c267d85
Siponek <szinkowicz@gmail.com>
2023-09-07 19:16:12 +0200

    add: regiresterd firestore repo

--- todoapp/lib/app.dart
+++ todoapp/lib/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todoapp/features/home/presentation/HomePage.dart';
+import 'package:todoapp/features/home/presentation/home_page.dart';
 import 'package:todoapp/features/login/data/repositories/authorization_repository.dart';
 import 'package:todoapp/features/login/presentation/pages/login_page/login_page.dart';
+import 'package:todoapp/services/repository_firestore.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,7 +13,11 @@ class MyApp extends StatelessWidget {
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider<AuthorizationRepository>(
-            create: (context) => AuthorizationRepository()),
+          create: (context) => AuthorizationRepository(),
+        ),
+        RepositoryProvider<RepositoryFirestore>(
+          create: (context) => RepositoryFirestore(),
+        ),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
@@ -20,8 +25,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        // home: const HomePage(title: 'Siemanko!'),
-        home: const LoginPage(),
+        home: const HomePage(title: 'Temporary no user!'),
+        // home: const LoginPage(),
         routes: {
           HomePage.route: (context) =>
               const HomePage(title: 'Siemanko, to /home!'),
