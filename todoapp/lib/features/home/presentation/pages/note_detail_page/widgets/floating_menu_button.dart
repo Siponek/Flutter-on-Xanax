ec6919e8c5f06390228d46afb5a2134528db139f
Siponek <szinkowicz@gmail.com>
2023-09-21 20:17:53 +0200

    feat: opening map on button

--- todoapp/lib/features/home/presentation/pages/note_detail_page/widgets/floating_menu_button.dart
+++ todoapp/lib/features/home/presentation/pages/note_detail_page/widgets/floating_menu_button.dart
@@ -1,12 +1,29 @@
 import 'dart:developer';
-
+import 'package:todoapp/features/home/domain/entities/note_entity.dart';
+import 'package:todoapp/features/home/presentation/pages/note_detail_page/note_detail_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 
 class FloatingMenuButton extends StatefulWidget {
-  const FloatingMenuButton({Key? key}) : super(key: key);
+  const FloatingMenuButton({
+    Key? key,
+    required this.note,
+  }) : super(key: key);
+
+  final NoteEntity note;
 
   @override
   _FloatingMenuButtonState createState() => _FloatingMenuButtonState();
+}
+
+Future<void> _launchMaps(double latitude, double longitude) async {
+  final url = Uri.parse(
+      'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }
 
 class _FloatingMenuButtonState extends State<FloatingMenuButton> {
@@ -14,12 +31,13 @@ class _FloatingMenuButtonState extends State<FloatingMenuButton> {
 
   @override
   Widget build(BuildContext context) {
+    final double halfOfDeviceWidth = MediaQuery.sizeOf(context).width / 2;
     return Stack(
-      alignment: Alignment.bottomRight,
+      // alignment: Alignment.bottomCenter,
       children: [
         AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
-          right: isMenuOpen ? -60 : 0,
+          right: isMenuOpen ? halfOfDeviceWidth - 60 - 44 : halfOfDeviceWidth,
           bottom: 0,
           curve: Curves.bounceInOut,
           child: Visibility(
@@ -37,7 +55,7 @@ class _FloatingMenuButtonState extends State<FloatingMenuButton> {
         ),
         AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
-          right: isMenuOpen ? 60 : 0,
+          right: isMenuOpen ? 60 + halfOfDeviceWidth - 44 : halfOfDeviceWidth,
           bottom: 0,
           curve: Curves.bounceInOut,
           child: Visibility(
@@ -55,7 +73,7 @@ class _FloatingMenuButtonState extends State<FloatingMenuButton> {
         ),
         AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
-          right: 0,
+          right: halfOfDeviceWidth - 44,
           bottom: isMenuOpen ? 60 : 0,
           curve: Curves.bounceInOut,
           child: Visibility(
@@ -65,15 +83,17 @@ class _FloatingMenuButtonState extends State<FloatingMenuButton> {
               mini: true,
               onPressed: () {
                 // Send action
-                log("Pressed send button", name: 'FloatingMenuButton');
+                log("Pressed map button", name: 'FloatingMenuButton');
+                _launchMaps(widget.note.location!.latitude,
+                    widget.note.location!.longitude);
               },
-              child: const Icon(Icons.send),
+              child: const Icon(Icons.map),
             ),
           ),
         ),
         AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
-          right: 0,
+          right: halfOfDeviceWidth - 48,
           bottom: 0,
           curve: Curves.bounceInOut,
           child: FloatingActionButton(
